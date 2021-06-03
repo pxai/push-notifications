c655ad85e778260dae1e1fb2a4b5f79695f1faad
pxai <pello.altadill@gluckgames.com>
2021-06-03 19:49:17 +0200

    Almost there

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', (event) => {
   let notification = event.data.json();
-  console.log("Are we here or not??? ay");
+  console.log("Are we here or not??? ay", JSON.stringify(event));
   self.registration.showNotification(
     notification.title,
     notification.options
